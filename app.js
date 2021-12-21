9b1183fa6b391618cafff6590c9119791e5796e6
aditya jani <adi07rak@gmail.com>
2021-12-20 19:03:43 -0600

    updated changes as requires.

---
+++ app.js
@@ -0,0 +1,5 @@
+// const logoInfo = document.querySelectorAll("#logo path");
+
+// for(let i=0; i<logoInfo.length;i++) {
+//     console.log('info of', i, 'value', logoInfo[i].getTotalLength());
+// }
